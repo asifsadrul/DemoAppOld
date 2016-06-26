8d25bea52f4909effdaf2cec48c296501460c486
pdfarhad <pdcsedu@gmail.com>
2016-06-26 16:40:14 +0600

    Asif Initial Commit

---
+++ DemoApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DemoApp
+//
+//  Created by Sadrul on 6/26/16.
+//  Copyright © 2016 nascenia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("hello world")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'DemoApp' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+pod 'Turbolinks', :git => 'https://github.com/turbolinks/turbolinks-ios.git'
+
+  # Pods for DemoApp
+
+end
